3575bf0bc0131c1729d5ba88350bf08234cb03e3
Ted Swedalla <fluffingthevoid@gmail.com>
2017-12-03 21:37:07 +0000

    automatically backed up by learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,5 +1,6 @@
 require_relative '../current_age_for_birth_year.rb'
 def current_age_for_birth_year(birth_year)
+  age_of_person = 2003 - birth_year
 end
 
 describe "current_age_for_birth_year method" do
